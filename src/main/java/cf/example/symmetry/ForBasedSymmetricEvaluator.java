63370f24cfc69369e929e8af9c73b330364c5316
Alexandru Jelescu <alexjelescu@gmail.com>
2022-03-23 15:37:09 +0200

    codul compileaza, testele nu merg

--- src/main/java/cf/example/symmetry/ForBasedSymmetricEvaluator.java
+++ src/main/java/cf/example/symmetry/ForBasedSymmetricEvaluator.java
@@ -1,23 +1,21 @@
 package cf.example.symmetry;
 
-import java.util.List;
-
 public class ForBasedSymmetricEvaluator extends ValidateString {
 
 
     public boolean evaluate(String str) {
-
+        boolean anyMatch = false;
 
         for (int i = 0; i < str.length(); i++) {
             char startCharFromReducedString = str.charAt(i);
             char endCharFromReducedString = str.charAt(str.length() - 1 - i);
-            return PairCreator.getRulePairs().stream()
+            anyMatch = PairCreator.getRulePairs().stream()
                     .anyMatch(characterCharacterPair -> characterCharacterPair.getLeft() == startCharFromReducedString
                             && characterCharacterPair.getRight() == endCharFromReducedString);
         }
 
 
-        return true;
+        return anyMatch;
     }
 
 
--- src/main/java/cf/example/symmetry/PairCreator.java
+++ src/main/java/cf/example/symmetry/PairCreator.java
@@ -1,24 +1,57 @@
 package cf.example.symmetry;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class PairCreator {
 
-    public static List<Pair<Character, Character>> getRulePairs() {
+    public static List<Pair<Character, Character>> getRulePairs()  {
         List<Pair<Character, Character>> pairsToCheck = new ArrayList<>();
-        InputStream file = PairCreator.class.getClassLoader().getResourceAsStream("pairs.txt");
-        Scanner scanner = new Scanner(file);
-        String rule = scanner.nextLine();
+        InputStream file = PairCreator.class
+                .getClassLoader()
+                .getResourceAsStream("pairs.txt");
 
-        for (int i = 0; i < rule.length(); i += 2) {
-            Pair pair = new Pair<>(rule.charAt(i), rule.charAt(i + 1));
-            pairsToCheck.add(pair);
+        String rule;
+
+        try {
+            assert file != null;
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(file, StandardCharsets.UTF_8))) {
+
+
+                while ((rule = br.readLine()) != null) {
+                    for (int i = 0; i < rule.length(); i += 2) {
+                        Pair<Character, Character> pair = new Pair<>(rule.charAt(i), rule.charAt(i + 1));
+                        pairsToCheck.add(pair);
+                    }
+                }
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return pairsToCheck;
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
